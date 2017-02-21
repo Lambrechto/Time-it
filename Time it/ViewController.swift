443fe336b31c064042731cedfc74b8a4da1aaedf
Lambrechto <daniellambrecht@macbook-pro.local>
2017-02-21 17:37:02 +0100

    Initial Commit

---
+++ Time it/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time it
+//
+//  Created by Daniel Lambrecht on 21/02/2017.
+//  Copyright © 2017 Lambrecht Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
