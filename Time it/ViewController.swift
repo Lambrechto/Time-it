a83ba9d35b5134af44617ef983cd91def26e05ed
Lambrechto <daniellambrecht@macbook-pro.local>
2017-02-21 18:10:35 +0100

    Beginning: With views and rounded corners

--- Time it/ViewController.swift
+++ Time it/ViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    let layerRadius: CGFloat = 6
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Rounded corners on views
+        view1.layer.cornerRadius = layerRadius
+        view2.layer.cornerRadius = layerRadius
+        view3.layer.cornerRadius = layerRadius
+        view4.layer.cornerRadius = layerRadius
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
